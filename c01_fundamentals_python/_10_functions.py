c3bd46de1fab58b805d22d9c53b38f62fdac1d68
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-01 23:30:42 -0600

    Definicion de funciones.

---
+++ c01_fundamentals_python/_10_functions.py
@@ -0,0 +1,36 @@
+"""
+FUNCIONES
+
+Son bloques de código reutilizables. Permiten además organizar el código de manera
+que es más fácil de mantener.
+"""
+
+# Define a new number type.
+Number = int | float
+
+
+def show_welcome_message():
+    print('Welcome to Python Programming Language')
+
+
+def get_rectangle_area(length: Number, width: Number) -> Number:
+    """
+    Calcula el área de un rectángulo dado su largo y ancho. Retorna -1
+    si se ingresan valores inválidos.
+    """
+    if length < 0 or width < 0:
+        error_msg = f'Error: length "{length}" and width: "{width}" must be >= 0.'
+        raise ValueError(error_msg)
+
+    return length * width
+
+
+if __name__ == '__main__':
+    show_welcome_message()
+
+    try:
+        print(get_rectangle_area(1, 2))
+        print(get_rectangle_area(1.6, 1_000.45))
+        print(get_rectangle_area(1, -2))
+    except ValueError as e:
+        print(e)
