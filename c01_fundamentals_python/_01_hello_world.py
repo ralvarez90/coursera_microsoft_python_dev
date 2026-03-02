48247d6ef5854dc057b1fa0c2719c7ab8727c648
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-01 23:16:36 -0600

    Hola mundo!

---
+++ c01_fundamentals_python/_01_hello_world.py
@@ -0,0 +1,14 @@
+"""
+HELLO WORLD
+
+Usamos la función print para enviar mensajes por consola.
+"""
+
+
+def main():
+    hello_msg: str = 'Hello World in Python'
+    print(hello_msg)
+
+
+if __name__ == '__main__':
+    main()
