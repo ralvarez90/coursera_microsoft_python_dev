47fb9b865e8208caff05063a449cdf9b3dc15061
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-01 23:31:12 -0600

    Introduccion a las clases.

---
+++ c01_fundamentals_python/_11_classes.py
@@ -0,0 +1,22 @@
+"""
+CLASES
+
+Son estructuras de comportamiento y estado a partir de las cuales podemos
+crear objetos con estado y comportamiento inicial.
+
+Son prácticamente entidades digitales que permiten definir el comportamiento
+de objetos.
+"""
+
+
+class SomeClassName:
+
+    def __init__(self):
+        self.msg = 'Message in SomeClassName instance'
+
+    def show_message(self):
+        print(self.msg)
+
+
+if __name__ == '__main__':
+    SomeClassName().show_message()
