98bd5b2a179174d1c40073865cdfe17eb8f4213a
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 11:37:37 -0600

    Tareas automatizables.

---
+++ c03_automation_and_scripting/_05_automation_kit.py
@@ -0,0 +1,17 @@
+"""
+TAREAS AUTOMATIZABLES
+
+La belleza de la automatización es trabajar de forma más inteligente, no con más
+ahínco. Permite liberar tiempo y energía mental para que puedas concentrarte en
+lo que realmente importa.
+
+Características Potenciales de Automatización
+1. Tareas repetitivas
+2. Tareas de alta frecuencia
+3. Tareas propensas a errores
+
+Otras características que indican potencialmente la automatización de tareas son:
+1. Tareas que involucren cálculos complejos o repetitivos
+2. Se requiere interacción con múltiples sistemas o uso de APIs
+3. Se puede beneficiar del análisis de datos y ML.
+"""
