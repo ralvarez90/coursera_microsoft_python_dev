3bb12740c78abc6f93028c3a8d915372eebcff2b
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 11:27:23 -0600

    Herramientas de automatizaicon.

---
+++ c03_automation_and_scripting/_02_tools.py
@@ -0,0 +1,37 @@
+"""
+HERRAMIENTAS DE AUTOMATIZACIÓN
+
+Dentro de las ventajas de la automatización tenemos:
+- Mejora la eficiencia y productividad
+- Mejora la exactitud y confiabilidad.
+- Permiten escalar de manera más rápida.
+
+La automatización analiza el contenido, señala los problemas y garantiza un
+entorno seguro incluso a medida que la plataforma o proyecto se expanda.
+
+En resumen, la automatización representa:
+1. Eficiencia
+2. Precisión
+3. Escalabilidad
+
+Dentro de las herramientas de automatización tenemos:
+
+1. Task Scheduler (Programador de Tareas - Windows)
+
+2. Crontab (Programador de Tareas - Linux, MAC)
+
+3. Selenium
+Framework de automatización web.
+
+4. BeautifulSoup
+Herramienta para analizar páginas web.
+
+5. Requests
+Librería para realizar peticiones http.
+
+6. Schedule
+Permite programar tareas sencillas
+
+7. PyAutoGui
+Permite controlar el ratón y el teclado mediante programación.
+"""
