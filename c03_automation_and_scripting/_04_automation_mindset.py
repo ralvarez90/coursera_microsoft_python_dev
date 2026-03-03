956e411fa9351db9de39401fb85f5da4dca3b8ce
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 11:37:09 -0600

    Mindset de automatizacion.

---
+++ c03_automation_and_scripting/_04_automation_mindset.py
@@ -0,0 +1,38 @@
+"""
+MENTALIDAD DE AUTOMATIZACIÓN: IDENTIFICAR OPORTUNIDADES
+
+Básicamente, consiste en identificar tareas y procesos que se pueden
+eficientar mediante la implementación de programas en Python.
+
+La mentalidad de automatización consiste en buscar una forma proactiva
+de usar la tecnología para hacer que las tareas en la vida laboral
+sea más fácil e impactante.
+
+Características clave:
+- Curiosidad y Cuestionamiento: '¿Se puede automatizar esto?', '¿Hay alguna
+forma mejor de hacer esto?'
+- Habilidad para resolver problemas.
+- Atención al detalle
+- Aprendizaje continuo
+
+Es importante comprender las complejidades de una tarea, identificar
+patrones y anticipar posibles casos extremos.
+"""
+
+lesson: str = """
+1. Buscar patrones
+Las tareas repetitivas suelen tener secuencias o estructuras predecibles. Si te das
+cuenta que haces lo mismo una y otra vez, es una buena señal de que python lo puede
+hacer por t.
+
+2. Acciones Repetitivas
+Otra señal reveladora de potencial de automatización son las tareas repetitivas. Si
+realizas constantemente las mismas acciones, como cambiar nombres, convertir formatos
+de datos o enviar corres estandarizados.
+
+3. Búsqueda de Áreas de Oportunidad
+La mentalidad de automatización también lo alienta a buscar formas de mejorar los procesos
+y los flujos de trabajo.
+"""
+
+print(lesson)
