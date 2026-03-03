711703d0b26f024ba5125caf7cdfb0a7bae802d1
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-02 21:46:05 -0600

    Scope o alcanze.

---
+++ c01_fundamentals_python/_14_scope.py
@@ -0,0 +1,28 @@
+"""
+SCOPE (ÁMBITO O ALCANCE)
+
+El scope o alcance es el acceso a datos dentro de nuestro código. Existe variables, funciones, objetos
+y clases que están accesibles en el scope global, existe además el scope local dentro de métodos y
+funciones.
+
+Notas:
+Los bloques if, while y try NO crean un nuevo scope, es decir, los bloques de control de flujo (bucles,
+condicionales y manejos de error) comparten el mismo scope del entorno en el que se encuentran (ya sea
+un scope global o el de una función)
+"""
+
+# Global variable
+COMPANY_NAME = 'Global Tech Solutions'
+
+
+def print_welcome_msg():
+    print(f'Welcome to {COMPANY_NAME.upper()}')
+
+
+def print_contact_info():
+    print(f'Contact {COMPANY_NAME} at info@globalsolutions.com')
+
+
+if __name__ == '__main__':
+    print_welcome_msg()
+    print_contact_info()
