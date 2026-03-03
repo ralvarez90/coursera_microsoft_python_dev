5574a6825b480c40629c9794a4e97166ded1c01e
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 14:35:29 -0600

    Ejcucion de scripts.

---
+++ c03_automation_and_scripting/_13_scripts.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+SCRIPTS
+
+Para ejecutar un script de python, usamos el siguiente comando:
+python script_name.py
+"""
+import argparse
+
+# Trigger a string output from the terminal
+print('Hello from the command line!')
+
+# Instantiate the Argparse package
+parser = argparse.ArgumentParser()
+
+# Create an argument called name.
+parser.add_argument('--name', help='Creates a ne name.')
+args = parser.parse_args()
+
+# Output the name
+if args.name:
+    print(f'My name is {args.name}')
+else:
+    print('Hello!')
