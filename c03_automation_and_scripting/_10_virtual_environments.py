d5688293e6a5fcdec4b1622fd68be5562174fa12
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 14:31:01 -0600

    Entornos virtuales.

---
+++ c03_automation_and_scripting/_10_virtual_environments.py
@@ -0,0 +1,36 @@
+"""
+ENTORNOS VIRTUALES
+
+En esencia los entornos virtuales dentro del ecosistema de Python ofrecen:
+
+Aislamiento:
+Piense en cada entorno virtual como una burbuja autocontenida. Dentro de esta burbuja, las dependencias de
+tu proyecto -las bibliotecas y paquetes específicos en los que se basa- están perfectamente aisladas del
+entorno global de Python y de otros proyectos. Esto previene conflictos de versiones y asegura que tu proyecto
+funcione consistentemente, independientemente de cualquier cambio realizado en otros entornos.
+
+Flexibilidad:
+Con los entornos virtuales, tienes la libertad de utilizar diferentes versiones de librerías y paquetes
+para diferentes proyectos. Esta flexibilidad es crucial cuando se trabaja en proyectos con distintos
+requisitos. Por ejemplo, un proyecto puede necesitar una versión antigua de una biblioteca por razones
+de compatibilidad, mientras que otro puede exigir la última versión por sus características de vanguardia.
+Los entornos virtuales permiten adaptar las dependencias a las necesidades específicas de cada proyecto
+sin que se produzcan conflictos.
+
+Reproducibilidad:
+Los entornos virtuales garantizan que su proyecto se comporte de forma coherente en diferentes máquinas.
+Al capturar las dependencias precisas dentro del entorno, puede reproducir fácilmente la configuración
+del proyecto en otro ordenador, ya sea la máquina de su colega, un servidor de pruebas o un entorno de
+producción. Esta reproducibilidad elimina el frustrante problema de "funciona en mi máquina" y garantiza
+una transición fluida del desarrollo a la implantación.
+
+Tranquilidad:
+Con los entornos virtuales, puede experimentar libremente y colaborar con confianza. Puede probar nuevas
+bibliotecas, probar diferentes versiones y explorar enfoques innovadores sin preocuparse por romper sus
+proyectos existentes o la instalación de Python del sistema. Esta libertad para experimentar fomenta
+la creatividad y la innovación. Además, los entornos virtuales simplifican la colaboración al proporcionar
+un entorno claro y coherente en el que pueden trabajar todos los miembros del equipo, lo que reduce el
+riesgo de conflictos y garantiza que todos estén de acuerdo.
+
+------------------------------------------------------------------------------------------------------------------------
+"""
