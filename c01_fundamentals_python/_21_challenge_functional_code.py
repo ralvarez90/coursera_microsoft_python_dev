4cc6313a46ab76037f1e01274612427d98b44b06
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-02 21:52:46 -0600

    Reto sobre funciones.

---
+++ c01_fundamentals_python/_21_challenge_functional_code.py
@@ -0,0 +1,38 @@
+"""
+RETO FUNCIONES
+
+Escribir tres funciones Python que implementen una sencilla herramienta de 
+conversión de temperatura. Ten cuidado de escribir correctamente los
+nombres de los métodos.
+"""
+from enum import Enum
+
+# Types
+number = int | float
+
+
+class TemperatureUnit(Enum):
+    C = 1
+    F = 2
+
+
+def convert_temperature(temperature: number, unit: TemperatureUnit):
+    match unit:
+        case TemperatureUnit.C:
+            return temperature * 9 / 5 + 32
+        case TemperatureUnit.F:
+            return (temperature - 32) * 5 / 9
+
+
+if __name__ == '__main__':
+    # Define temperatures
+    temperatureC: number = 25
+    temperatureF: number = 77
+
+    # Get converted temperatures
+    convertedF = convert_temperature(temperatureC, TemperatureUnit.C)
+    convertedC = convert_temperature(temperatureF, TemperatureUnit.F)
+
+    # Show results
+    print(f'{temperatureC:.2f}°C is equal to {convertedF:.2f}°F')
+    print(f'{temperatureF:.2f}°F is equal to {convertedC:.2f}°C')
