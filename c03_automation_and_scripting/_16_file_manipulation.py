6c28a504e3920302061df357eb4d86f1548803cb
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 15:05:27 -0600

    Introduccion a la manipulacion de archivos.

---
+++ c03_automation_and_scripting/_16_file_manipulation.py
@@ -0,0 +1,12 @@
+"""
+MANIPULACIÓN DE ARCHIVOS
+
+Con python podemos manipular archivos de manera automatizada. Esto permite
+optimizar flujos de trabajo.
+
+Dentro de las manipulaciones de archivos comunes tenemos:
+- Lectura y escritura de archivos
+- Renombramiento y organización de archivos.
+- Data Transformation (transformaciones de datos)
+- Compresión de archivos y archivado.
+"""
