dc0c0ec6251f88faebb19b7bc3f52056332b3b9c
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 11:26:55 -0600

    Introduccion a la automatizacion.

---
+++ c03_automation_and_scripting/_01_intro.py
@@ -0,0 +1,8 @@
+"""
+INTRODUCCIÓN A LA AUTOMATIZACIÓN
+
+La automatización en Python es el uso de scripts y programas para realizar tareas
+repetitivas, manuales o de gran volumen sin intervención humana constante. En lugar
+de hacer clic manualmente en archivos, copiar datos o navegar por sitios web,
+escribes código que le indica a la computadora exactamente qué pasos seguir.
+"""
