0f7c93349addefe781b60ff8e940a0fa4574f187
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 11:38:00 -0600

    Preguntas claves.

---
+++ c03_automation_and_scripting/_06_questions_before.py
@@ -0,0 +1,29 @@
+"""
+10 PREGUNTAS PREVIAS A LA AUTOMATIZACIÓN
+
+1. ¿Con qué frecuencia se realiza la tarea?
+
+2. ¿Cuál es la complejidad de la tarea?
+
+3. ¿Cuál es el Retorno de la inversión (ROI) potencial?
+Es importante calcular cuidadosamente el tiempo, los recursos y los costes asociados a la
+automatización de la tarea. Al mismo tiempo, hay que considerar los beneficios potenciales,
+como el ahorro de costes conseguido gracias a la reducción de mano de obra o el aumento de
+ingresos generado por la mejora de la eficiencia. La creación de una hoja de cálculo para
+realizar un seguimiento de estos costes y beneficios estimados puede proporcionar una imagen
+más clara del ROI potencial, ayudándole a tomar una decisión informada sobre si proceder
+con la automatización.
+
+4. ¿Hasta qué punto es propenso a errores el proceso manual?
+5. ¿La tarea requiere interacción humana o toma de decisiones?
+
+6. ¿La tarea está estandarizada y es predecible?
+Las tareas con pasos claros y bien definidos y pocas excepciones son candidatas ideales para
+la automatización.
+
+7. ¿Se dispone de los datos y recursos necesarios?
+8. ¿Cuáles son los posibles riesgos e inconvenientes?
+
+9. ¿Cómo afectará la automatización a los empleados?
+10. ¿Está la tecnología disponible y suficientemente madura?
+"""
