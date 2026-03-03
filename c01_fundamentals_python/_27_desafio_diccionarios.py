28774ab6e375dd627439ae729e5f67871dc5bd88
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 11:16:29 -0600

    Reto diccionarios.

---
+++ c01_fundamentals_python/_27_desafio_diccionarios.py
@@ -0,0 +1,38 @@
+"""
+_27_desafío_diccionarios.py
+"""
+
+
+class Product:
+
+    def __init__(self, name: str, price: float, quantity: int):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def to_dict(self) -> dict:
+        return {
+            'name': self.name,
+            'price': self.price,
+            'quantity': self.quantity,
+        }
+
+
+products = {
+    'SKU123': Product('Widget A', 19.99, 50).to_dict(),
+    'SKU456': Product('Gadget B', 34.95, 25).to_dict(),
+    'SKU789': Product('Gizmo C', 9.99, 100_1000).to_dict(),
+}
+
+# Search product
+sku_to_find = 'SKU123'
+for sku in products.keys():
+    if sku == sku_to_find:
+        print('-' * 50)
+        print(products[sku])
+        print(f'The price of {products[sku]["name"]} is ${products[sku]["price"]:,.2f}')
+        print('-' * 50)
+
+# Show all products
+for product in products.values():
+    print(f'- {product}')
