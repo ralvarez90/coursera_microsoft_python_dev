e69d8bd37bcc8e316b8d0c6016a410242e6e838a
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-02 21:54:23 -0600

    Introduccion a diccionarios.

---
+++ c01_fundamentals_python/_25_challenge_dict_student.py
@@ -0,0 +1,20 @@
+"""
+RETO GENERACIÓN DE DICCIONARIO
+
+Generar diccionario con información de un estudiante.
+"""
+from pprint import pprint
+
+# Dictionary definition
+some_student = {
+    'name': 'Ana García',
+    'age': 16,
+    'address': 'Calle Falsa 123',
+    'esta_inscrito': True,
+    'calificaciones': {'maths': 9.0, 'geography': 10},
+    'house_coords': (123.123, 123.123),
+    'intereses': ['Música', 'Futbol']
+}
+
+# Show dict
+pprint(some_student)
