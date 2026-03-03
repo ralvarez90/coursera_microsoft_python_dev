bee45a0a160153558d8273cb57637c1b7bd7045c
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 14:30:09 -0600

    De ideas a la implementacion.

---
+++ c03_automation_and_scripting/_07_from_idea_to_implementation.py
@@ -0,0 +1,16 @@
+"""
+DE LA IDEA A LA IMPLEMENTACIÓN
+
+Identificamos de la siguiente manera oportunidades de automatización.
+
+1. Viabilidad (Feasibility)
+La automatización se nutre de las tareas que siguen un patrón establecido. Si la
+tarea es ambígua o requiere muchos juicios humanos, es posible que no sea la
+mejor opción para la automatización.
+
+2. Datos
+La tarea que se piensa automatizar ¿se basa en datos fácilmente disponibles y
+accesibles?
+
+3. Evaluar Skills y Recursos
+"""
