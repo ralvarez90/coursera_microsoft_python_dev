4116ab6fd2cbdc15f01432659bd215e7527c9afb
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 11:27:50 -0600

    RPA (boots).

---
+++ c03_automation_and_scripting/_03_rpa_boots.py
@@ -0,0 +1,20 @@
+"""
+ROBOTIC PROCESS AUTOMATION
+
+Automatización Robótica de Procesos es una tecnología de software que permite crear, desplegar
+y gestionar "robots" de software que emulan las acciones de un ser humano interactuando con sistemas
+digitales y software.
+
+A diferencia de la automatización tradicional basada en APIs o código puro, el RPA "observa" lo que
+un usuario hace en la interfaz gráfica (clics, escribir, copiar y pegar) y repite esas tareas de
+forma automática, precisa y a gran escala.
+
+En esencia, la RPA emplea "robots" de software para emular acciones humanas en entornos digitales.
+Estos robots se conectan sin problemas a las aplicaciones, navegan por los menús, extraen y procesan
+datos, rellenan formularios e incluso realizan cálculos con una precisión inquebrantable y sin
+necesidad de descansos ni de dormir.
+
+Cuando la RPA une fuerzas con la flexibilidad de los scripts de Python, su potencial se expande
+exponencialmente. Escenarios de automatización complejos que antes parecían insuperables se
+vuelven alcanzables, abriendo las puertas a nuevos niveles de innovación y optimización.
+"""
