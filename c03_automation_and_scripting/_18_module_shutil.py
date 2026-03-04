88da95b8054a037c8f95a0957cf64f0de6cbb55b
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 22:45:58 -0600

    Modulo shutil.

---
+++ c03_automation_and_scripting/_18_module_shutil.py
@@ -0,0 +1,21 @@
+"""
+MÓDULO SHUTIL (SHELL UTILITIES)
+
+Mientras que el módulo os proporciona las herramientas básicas para interactuar con el
+sistema de archivos, shutil va un paso más allá, ofreciendo cómodos atajos para las
+tareas más comunes. Es como tener un bibliotecario que puede copiar rápidamente
+estanterías enteras, moverlas a nuevas ubicaciones o incluso borrar secciones
+obsoletas de la biblioteca.
+
+shutil.copy()
+Permite copiar un archivo.
+
+shutil.copytree()
+Permite copiar un directorio y su contenido.
+
+shutil.move()
+Permite mover un archivo o directorio dentro del sistema de archivos.
+
+shutil.rmtree()
+Permite borrar un directorio junto con su contenido.
+"""
