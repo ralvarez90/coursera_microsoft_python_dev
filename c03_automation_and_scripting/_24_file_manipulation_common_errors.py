1df0f1f9db5790d545f0da3883f997b1cff5bd41
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 22:49:37 -0600

    Errores comunes trabajando archivos.

---
+++ c03_automation_and_scripting/_24_file_manipulation_common_errors.py
@@ -0,0 +1,17 @@
+"""
+ERRORES COMUNES
+
+1. Rutas de Archivos/Directorios Incorrectas
+2. Problemas de Permisos
+3. Errores de Codificación
+4. Modo de archivo incorrecto
+5. Fuga de recursos (leaks resources (leak memory))
+
+Para prevenir este tipo de errores, tenga en cuenta.
+- Lea los mensajes de error y su tipo.
+- Verifique las rutas de archivos, y si está usando rutas absolutas o relativas.
+- Especifique correctamente el tipo de codificación.
+- Utilize siempre la sentencia with.
+- Use nombres de variables descriptivos.
+- Use los bloques de manejos de errores try-except-else-finally.
+"""
