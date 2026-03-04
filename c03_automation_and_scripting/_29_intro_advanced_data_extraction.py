b196576eb067bafef15981d524c79aad1e67382d
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2026-03-03 22:59:24 -0600

    Introduccion a alternativas de extraccion de datos.

---
+++ c03_automation_and_scripting/_29_intro_advanced_data_extraction.py
@@ -0,0 +1,47 @@
+"""
+EXTRACCIÓN AVANZADA DE DATOS
+
+La extracción de datos es el proceso de recuperación de información específica de varias
+fuentes, transformando los datos no estructurados sin procesar en un formato estructurado
+para el análisis y toma de decisiones.
+
+Más allá de la coincidencia de patrones (pattern matching), las técnicas avanzadas permiten
+extraer información aún más matizada de sus datos.
+
+NLP (Natural Language Processing) y Modelos de Machine Learning para extraer información
+de texto no estructurado.
+
+1. Natural Language Processing
+El NLP es la disciplina que permite a las máquinas "entender" el lenguaje humano. En
+la extracción de información, su función es descomponer y analizar la estructura del
+texto para que el código sepa qué partes son relevantes.
+
+Dentro de la extracción de información, el NLP es la disciplina que permite a las
+máquinas "entender" el lenguaje humano. En la extracción de información, su función
+es descomponer y analizar la estructura del texto para que el código sepa qué
+partes son relevantes.
+
+Dentro de las tareas clave del NLP en extracción tenemos:
+- Tokenización: Dividir texto en unidades más pequeñas (palabras o frases)
+- Segmentación de Oraciones: Identificar donde termina una idea e inicia otra.
+- POS Tagging: Post etiquetado donde se identifica si una palabra es eun
+sustantivo, verbo o adjetivo.
+
+Mientras que el NLP analiza la gramática, el Machine Learning se encarga de
+aprender patrones para tomar decisiones sin estar programado con reglas
+rígidas (como miles de sentencias if-else).
+
+Existen otras técnicas para la extracción de información, ofreciendo ventajas
+según la fuente de datos y los requisitos específicos.
+
+1. Web Scraping
+Permite recopilar información directamente sobre un sitio web. Es ideal para
+tareas como recopilar datos de productos de tiendas en línea.
+
+2. Interacciones con APIs.
+
+3. SQL Queries (Consultas SQL)
+Si los datos ya están almacenados en una base de datos, puede usar consultas
+de SQL. Esto le permite filtrar, ordenar y agregar en función de criterios
+específicos.
+"""
